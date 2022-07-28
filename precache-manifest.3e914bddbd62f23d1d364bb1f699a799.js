512afeecfe031271d8e1aeca384de91519ac69b2
Prapti Rana <64392255+prapti410@users.noreply.github.com>
2022-07-29 00:03:48 +0530

    Updates

---
+++ precache-manifest.3e914bddbd62f23d1d364bb1f699a799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11dc5e8a43be2938e76e",
+    "url": "/TextUtils-React/static/css/main.6396ecec.chunk.css"
+  },
+  {
+    "revision": "11dc5e8a43be2938e76e",
+    "url": "/TextUtils-React/static/js/main.11dc5e8a.chunk.js"
+  },
+  {
+    "revision": "833a7611ce42bb2c9054",
+    "url": "/TextUtils-React/static/js/1.833a7611.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils-React/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "12ed300bc04512e0ffd6",
+    "url": "/TextUtils-React/static/js/runtime~main.12ed300b.js"
+  },
+  {
+    "revision": "d45281126d8f0810f192ed842496f3e5",
+    "url": "/TextUtils-React/index.html"
+  }
+];
